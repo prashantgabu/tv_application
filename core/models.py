7a2e878c4dd065f98b9a6fc21998f62db68e184b
Prashant Gabu <prashantg5399@gmail.com>
2022-09-04 14:16:11 +0530

    Updating apis

--- core/models.py
+++ core/models.py
@@ -1,21 +1,24 @@
 from django.db import models
 from fcm_django.models import FCMDevice
 import json
+
 # Create your models here.
 DEFAULT_LOCK_NOTE = 'Your Service Is Expired.'
+
 
 class RemoteDevice(models.Model):
     lock = models.BooleanField(default=True)
     device = models.ForeignKey(FCMDevice, on_delete=models.CASCADE)
-    note = models.TextField(default=DEFAULT_LOCK_NOTE,null=True)
+    note = models.TextField(default=DEFAULT_LOCK_NOTE, null=True)
+    mac_address = models.CharField(max_length=300, null=True, blank=True, verbose_name="MAC Address")
 
     def __str__(self):
         if self.device:
             if self.device.name:
-               return self.device.name+' '+'('+self.device.device_id+')'
+                return self.device.name + ' ' + '(' + self.device.device_id + ')'
             else:
                 return self.device.device_id
-        return 'Unknown Device'+' '+'('+self.id+')'
+        return 'Unknown Device' + ' ' + '(' + self.id + ')'
 
     def save(self, *args, **kwargs):
         super(RemoteDevice, self).save(*args, **kwargs)
@@ -26,10 +29,10 @@ class RemoteDevice(models.Model):
             # new
             pass
         data = {
-            "action":"lock",
-            "bundle":{
+            "action": "lock",
+            "bundle": {
                 "lock": self.lock,
                 "note": self.note,
             }
         }
-        self.device.send_message(data={"data":json.dumps(data)})
+        self.device.send_message(data={"data": json.dumps(data)})
